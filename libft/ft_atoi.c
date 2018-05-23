bd406bced7cc647f085270283bc1fca2c59f0257
diVid3 <egenis@student.wethinkcode.co.za>
2018-05-23 17:06:03 +0200

    2018/05/23

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,16 +6,43 @@
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/23 11:33:38 by egenis            #+#    #+#             */
-/*   Updated: 2018/05/23 11:41:32 by egenis           ###   ########.fr       */
+/*   Updated: 2018/05/23 17:05:36 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include "libft.h"
+#include <stdlib.h>
+
 int ft_atoi(const char *str)
 {
+	int ans;
+	int sign;
 
+	ans = 0;
+	sign = 1;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while ((*str >= '0' && *str <= '9') && *str)
+		ans = (ans * 10) + (*str++ - 48);
+	return (ans * sign);
 }
 
 int main(void)
 {
+	size_t cntr = 0;
+	char test_ar[4] = "013";
+	while (cntr <= 127)
+	{
+		test_ar[0] = cntr;
+		printf("Returned int for cntr %zu is %d\n", cntr, atoi(test_ar));
+		++cntr;
+	}
+	//printf("Returned int is %d\n", atoi("   \t\r5634"));
+	//printf("Returned int is %d\n", ft_atoi("  \t\r5634"));
 	return (0);
 }
