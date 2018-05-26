c0f27c948810cb8d392a078003bf22b3b7e46ff7
diVid3 <egenis@student.wethinkcode.co.za>
2018-05-26 17:30:30 +0200

    2018/05/26

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,11 +6,13 @@
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 16:42:28 by egenis            #+#    #+#             */
-/*   Updated: 2018/05/25 08:59:30 by egenis           ###   ########.fr       */
+/*   Updated: 2018/05/26 17:29:49 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -24,4 +26,14 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		++cntr;
 	}
 	return (dst);
+}
+
+int main(void)
+{
+	char buff1[30] = "Hello There Fellow!";
+	char buff2[30] = "Hello There Fellow!";
+	char src[30] = "LOLOLOL";
+	printf("%s\n", memmove(buff1, src, 5));
+	printf("%s\n", ft_memmove(buff2, src, 5));
+	return (0);
 }
