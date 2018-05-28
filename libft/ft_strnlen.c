d4bce5fd3f7f5ac4ee06dd2d9f09a3b78847013a
diVid3 <egenis@student.wethinkcode.co.za>
2018-05-28 15:05:32 +0200

    2018/05/28

--- libft/ft_strnlen.c
+++ libft/ft_strnlen.c
@@ -1,37 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strnlen.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: egenis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/19 14:28:06 by egenis            #+#    #+#             */
-/*   Updated: 2018/05/28 14:58:20 by egenis           ###   ########.fr       */
+/*   Created: 2018/05/28 14:58:41 by egenis            #+#    #+#             */
+/*   Updated: 2018/05/28 15:01:36 by egenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strnlen(const char *s, size_t maxlen)
 {
-	size_t	cntr;
-	size_t	nul_pos;
-	size_t	dst_len;
-	size_t	src_len;
-
-	cntr = 0;
-	nul_pos = ft_strlen(dst);
-	dst_len = nul_pos;
-	src_len = ft_strlen(src);
-	while (cntr < src_len)
-	{
-		dst[nul_pos] = src[cntr];
-		++nul_pos;
-		++cntr;
-	}
-	if (dstsize - (src_len + 1) > 0)
-		dst[src_len] = '\0';
-	return (dst_len + src_len + 1);
 }
 
+int main(void)
+{
 
+}
