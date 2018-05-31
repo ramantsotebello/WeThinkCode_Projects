6047db4d499dc02ae8a25442a1e33c4ef4a166ca
diVid3 <egenis@student.wethinkcode.co.za>
2018-05-31 05:21:11 +0200

    2018/05/31

---
+++ libft/ft_memdel.c
@@ -0,0 +1,12 @@
+
+
+#include "libft.h"
+
+void	ft_memdel(void **ap)
+{
+	if (ap && *ap)
+	{
+		free(*ap);
+		*ap = NULL;
+	}
+}
---
+++ libft/ft_strnew.c
@@ -0,0 +1,11 @@
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	
+}
+int		main(void)
+{
+	return (0);
+}
