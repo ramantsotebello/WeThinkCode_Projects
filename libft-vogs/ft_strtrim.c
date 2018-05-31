e8d8de4d175b451403f48a25d532aae8ed472314
diVid3 <egenis@student.wethinkcode.co.za>
2018-05-31 20:47:35 +0200

    2018/05/31

--- libft-vogs/ft_strtrim.c
+++ libft-vogs/ft_strtrim.c
@@ -12,11 +12,11 @@
 
 #include "libft.h"
 
+/*
 static	size_t	ft_calc_whitespaces(char const *s)
 {
 	size_t	cntr;
 	size_t	wht_spcs;
-
 
 	cntr = 0;
 	wht_spcs = 0;
@@ -56,6 +56,52 @@ char			*ft_strtrim(char const *s)
 		newstr[cntr] = s[cntr_s];
 		++cntr;
 		++cntr_s;
+	}
+	return (newstr);
+}
+*/
+
+static	size_t	ft_calc_strlen(char const *s)
+{
+	size_t	cntr;
+	size_t	str_len;
+
+	cntr = 0;
+	str_len = 0;
+	if (!s)
+		return (0);
+	while (s[cntr] && (s[cntr] == ' ' || s[cntr] == '\n'|| s[cntr] == '\t'))
+		++cntr;
+	while (s[cntr] && (s[cntr] != ' ' || s[cntr] != '\n'|| s[cntr] != '\t'))
+	{
+		++cntr;
+		++str_len;
+	}
+	return (str_len);
+}
+
+char			*ft_strtrim(char const *s)
+{
+	char	*newstr;
+	size_t	str_len;
+	size_t	cnt_s;
+	size_t	cnt_n;
+
+	if (!s)
+		Return (NULL);
+	str_len = ft_calc_strlen(s);
+	newstr = ft_strnew(str_len);
+	if !(newstr)
+		return (NULL);
+	cnt_s = 0;
+	cnt_n = 0;
+	while (s[cnt_s] && (s[cnt_s] == ' ' || s[cnt_s] == '\n'|| s[cnt_s] == '\t'))
+		++cnt_s;
+	while (s[cnt_s] && (s[cnt_s] != ' ' || s[cnt_s] != '\n'|| s[cnt_s] != '\t'))
+	{
+		newstr[cnt_n] = s[cnt_s];
+		++cnt_n;
+		++cnt_s;
 	}
 	return (newstr);
 }
